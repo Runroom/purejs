83fd425143dc9e274577e66a9d326c662dca3e5f
Italo <italo@runroom.com>
2017-08-22 17:43:17 +0200

    Update styles of GoTop module

---
+++ components/GoTop.js
@@ -0,0 +1,65 @@
+import ScrollTo from './ScrollTo';
+
+const pageHeight = document.body.clientHeight;
+const scrollOffset = 0.25;
+const maxScrollTop = pageHeight * scrollOffset;
+
+let scrollIsVisible = false;
+
+function handleScrollTopVisibility() {
+  if (window.scrollY >= maxScrollTop) {
+    if (!scrollIsVisible) {
+      createScrollTop();
+      scrollIsVisible = true;
+    }
+  } else {
+    if (scrollIsVisible) {
+      document.querySelector('.js-scrollTop').remove();
+      scrollIsVisible = false;
+    }
+  }
+}
+
+function createScrollTop() {
+  const styles = {
+    background: 'rgba(0, 0, 0, .5)',
+    border: '0',
+    bottom: '24px',
+    borderRadius: '50%',
+    display: 'block',
+    height: '48px',
+    position: 'fixed',
+    right: '24px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    width: '48px'
+  };
+
+  const content =
+    '<svg style="height: 32px; padding: 8px 0; width: 32px;"><path fill="#fff" d="M21.707 13.293l-5-5a.999.999 0 0 0-1.414 0l-5 5a.999.999 0 1 0 1.414 1.414L15 11.414V23a1 1 0 1 0 2 0V11.414l3.293 3.293a.997.997 0 0 0 1.414 0 .999.999 0 0 0 0-1.414z"/></svg>';
+
+  const element = document.createElement('a');
+
+  element.className = 'js-scrollTop';
+  Object.assign(element.style, styles);
+  element.innerHTML = content;
+
+  element.addEventListener('click', function(event) {
+    event.preventDefault();
+    ScrollTo(0);
+  });
+
+  document.body.appendChild(element);
+}
+
+function handleExtend(settings) {
+  opts = Object.assign({}, opts, settings);
+}
+
+export default function GoTop(settings) {
+  if (settings) {
+    handleExtend(settings);
+  }
+  handleScrollTopVisibility();
+  window.addEventListener('scroll', handleScrollTopVisibility);
+}
