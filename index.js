a1d2ffff4810c1226577a5dae4395c2d181cdf26
Italo <italo@runroom.com>
2017-08-23 12:27:09 +0200

    Add index as main

---
+++ index.js
@@ -0,0 +1,9 @@
+import Anchor, { handleScrollToAnchor } from './Anchor';
+import Cookies from './Cookies';
+import GoTop from './GoTop';
+import ScrollDirection from './ScrollDirection';
+import ScrollTo from './ScrollTo';
+import ScrollTop from './ScrollTop';
+import Touchable from './Touchable';
+
+export { Anchor, handleScrollToAnchor, Cookies, ScrollDirection, ScrollTo, ScrollTop, Touchable };
