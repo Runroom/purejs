562e6a5772636480b99cef0bc1af2320b1c502a2
Italo Devoto Ramella <italodr@gmail.com>
2018-01-19 11:55:18 +0100

    Working on coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -1,59 +1,71 @@
 // Karma configuration
 // Generated on Wed Jan 10 2018 09:56:25 GMT+0100 (CET)
-var configuration = {
-  basePath: '',
-  frameworks: ['mocha', 'sinon-chai', 'chai'],
-  files: ['lib/purejs.min.js', 'test/**/*.js'],
-  preprocessors: {
-    'lib/purejs.min.js': ['coverage'],
-    'test/**/*.js': ['babel']
-  },
-  babelPreprocessor: {
-    options: { presets: ['env'], sourceMap: 'inline' },
-    filename: function(file) {
-      return file.originalPath.replace(/\.js$/, '.es5.js');
-    },
-    sourceFileName: function(file) {
-      return file.originalPath;
-    }
-  },
-  reporters: ['mocha', 'coverage'],
-  coverageReporter: { reporters: [{ type: 'html', dir: 'build/coverage' }, { type: 'text-summary', dir: '/' }] },
-  port: 9876,
-  colors: true,
-  autoWatch: true,
-  browsers: ['Chrome'],
-  singleRun: false,
-  concurrency: Infinity,
-  client: {
-    chai: {
-      includeStack: true
-    }
-  },
-  customLaunchers: {
+const plugins = [
+  require('istanbul-instrumenter-loader'),
+  require('karma-babel-preprocessor'),
+  require('karma-chai'),
+  require('karma-chrome-launcher'),
+  require('karma-coverage'),
+  require('karma-coveralls'),
+  require('karma-mocha'),
+  require('karma-mocha-reporter'),
+  require('karma-sinon-chai')
+];
+const coverageReporters = [{ type: 'text-summary' }];
+const reporters = ['mocha', 'coverage'];
+
+let browsers = ['Chrome'];
+let customLaunchers = {};
+let singleRun = false;
+
+if (process.env.TRAVIS) {
+  browsers = ['Chrome', 'Chrome_travis_ci'];
+  reporters.push('coveralls');
+  coverageReporters.push({ type: 'lcov', dir: 'coverage' });
+  customLaunchers = {
     Chrome_travis_ci: {
       base: 'Chrome',
       flags: ['--no-sandbox']
     }
-  },
-  plugins: [
-    require('istanbul-instrumenter-loader'),
-    require('karma-babel-preprocessor'),
-    require('karma-chai'),
-    require('karma-chrome-launcher'),
-    require('karma-coverage'),
-    require('karma-mocha'),
-    require('karma-mocha-reporter'),
-    require('karma-sinon-chai')
-  ]
-};
-
-if (process.env.TRAVIS) {
-  configuration.browsers = ['Chrome_travis_ci'];
-  configuration.singleRun = true;
+  };
+  singleRun = true;
+} else {
+  coverageReporters.push({ type: 'html', dir: 'coverage', subdir: '.' });
 }
 
 module.exports = function(config) {
-  configuration.logLevel = config.LOG_ERROR;
-  config.set(configuration);
+  config.set({
+    basePath: '',
+    frameworks: ['mocha', 'sinon-chai', 'chai'],
+    files: ['lib/purejs.min.js', 'test/**/*.js'],
+    preprocessors: {
+      'lib/purejs.min.js': ['coverage'],
+      'test/**/*.js': ['babel']
+    },
+    babelPreprocessor: {
+      options: { presets: ['env'], sourceMap: 'inline' },
+      filename: function(file) {
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      },
+      sourceFileName: function(file) {
+        return file.originalPath;
+      }
+    },
+    reporters: reporters,
+    coverageReporter: { reporters: coverageReporters },
+    logLevel: config.LOG_ERROR,
+    port: 9876,
+    colors: true,
+    autoWatch: true,
+    browsers: browsers,
+    singleRun: singleRun,
+    concurrency: Infinity,
+    client: {
+      chai: {
+        includeStack: true
+      }
+    },
+    customLaunchers: customLaunchers,
+    plugins: plugins
+  });
 };
