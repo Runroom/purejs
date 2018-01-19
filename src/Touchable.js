5042ac1ac62fd6ddd44487e06999f51b4f3cd269
Italo Devoto Ramella <italodr@gmail.com>
2018-01-19 10:54:33 +0100

    Fix issues with file casing in Mac generating file duplication in repo

--- src/Touchable.js
+++
@@ -1,8 +0,0 @@
-export default function touchable(container) {
-  const touchsupport =
-    'ontouchstart' in window || window.navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
-  const touchClass = touchsupport ? 'touch' : 'non-touch';
-  let el = container;
-  if (!container) el = document.documentElement;
-  el.classList.add(touchClass);
-}
