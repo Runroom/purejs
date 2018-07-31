1a1dbc7bef738147fcbd774f2a779bc2a1acbb49
Italo Devoto Ramella <italodr@gmail.com>
2018-07-31 17:15:27 +0200

    Add conditional to prevent default class creation on DOM

--- src/touchable.js
+++ src/touchable.js
@@ -1,7 +1,9 @@
 export default function touchable(container = document.documentElement) {
   const touchsupport =
     'ontouchstart' in window || window.navigator.maxTouchPoints > 0 || window.navigator.msMaxTouchPoints > 0;
-  const touchClass = touchsupport ? 'touch' : 'non-touch';
-  container.classList.add(touchClass);
+  if (container) {
+    const touchClass = touchsupport ? 'touch' : 'non-touch';
+    container.classList.add(touchClass);
+  }
   return touchsupport;
 }
--- test/touchable.spec.js
+++ test/touchable.spec.js
@@ -2,6 +2,13 @@ describe('Touchable behavior', () => {
   it('should add non-touch class to html element', done => {
     purejs.touchable();
     expect(document.documentElement.className).to.have.string('non-touch');
+    document.documentElement.classList.remove('non-touch');
+    done();
+  });
+
+  it('should not add class to html element', done => {
+    purejs.touchable(false);
+    expect(document.documentElement.className).to.not.have.string('non-touch');
     done();
   });
 });
