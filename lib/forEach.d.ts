2d7f6f144c053cb8660a1d923a025b3a19a8176e
Italo Devoto Ramella <italodr@gmail.com>
2018-10-18 12:04:49 +0200

    update types for callbacks

--- lib/forEach.d.ts
+++ lib/forEach.d.ts
@@ -2,5 +2,5 @@
  * https://jsperf.com/for-vs-foreach/37
  * https://coderwall.com/p/kvzbpa/don-t-use-array-foreach-use-for-instead
  */
-declare function forEach(haystack: any[], callback: () => void, scope?: void): void;
+declare function forEach(haystack: any, callback: (value: any, index: number | string, haystack: any) => void, scope?: void): void;
 export default forEach;
--- src/lib/forEach.ts
+++ src/lib/forEach.ts
@@ -2,7 +2,11 @@
  * https://jsperf.com/for-vs-foreach/37
  * https://coderwall.com/p/kvzbpa/don-t-use-array-foreach-use-for-instead
  */
-function forEach(haystack: { [key: string]: any } | any[], callback: () => void, scope?: void) {
+function forEach(
+  haystack: any,
+  callback: (value: any, index: number | string, haystack: any) => void,
+  scope?: void
+) {
   if (Object.prototype.toString.call(haystack) === '[object Object]') {
     const keyHaystack = Object.keys(haystack) as any[];
     for (let i = 0, len = keyHaystack.length; i < len; i += 1) {
