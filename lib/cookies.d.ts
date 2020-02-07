745f5fcce2574b73f5b255f898e1a236402edcf6
Italo Devoto Ramella <italo@runroom.com>
2020-02-07 13:44:38 +0100

    Remove deprecated packages

--- lib/cookies.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: {
-    get: (name: string) => string;
-    set: (name: string, value?: string | number, days?: number) => void;
-    remove: (name: string) => void;
-};
-export default _default;
